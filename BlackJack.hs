ebed4f8147e7b3bdf12cf9a04e50ccfc6f3da7d3
Rasmus Rosengren <rasmus.rosengren@pm.me>
2019-11-18 17:32:46 +0100

    Add some comments

--- BlackJack.hs
+++ BlackJack.hs
@@ -29,6 +29,7 @@ display (Add card hand) = (displayCard card) ++ "\n" ++ (display hand)
 
 -- A2 -------------------------------------------------------------------------
 
+-- Get a numeric value for a rank
 valueRank :: Rank -> Integer
 valueRank (Numeric n) = n
 valueRank Jack = 10
@@ -36,26 +37,31 @@ valueRank Queen = 10
 valueRank King = 10
 valueRank Ace = 11
 
+-- Calculate total value for a given hand where ace is 11
 initialValue :: Hand -> Integer
 initialValue Empty = 0
 initialValue (Add card hand) = valueRank (rank card) + initialValue hand
 
+-- Calculate number of aces in a given hand
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add card hand) | rank card == Ace = 1 + numberOfAces hand
                              | otherwise        = 0 + numberOfAces hand
 
+-- Calculate the total value for a hand, accounting for ace value (11 or 1)
 value :: Hand -> Integer
 value hand | initialValue hand > 21 = initialValue hand - 10 * numberOfAces hand
            | otherwise              = initialValue hand
 
 -- A3 -------------------------------------------------------------------------
 
+-- Check if the game's over
 gameOver :: Hand -> Bool
 gameOver hand = value hand > 21
 
 -- A4 -------------------------------------------------------------------------
 
+-- Check which hand is the winner (guest or bank)
 winner :: Hand -> Hand -> Player
 winner guest _    | gameOver guest == True = Bank
 winner _ bank     | gameOver bank == True = Guest
@@ -64,6 +70,7 @@ winner guest bank | value guest > value bank = Guest
 
 -- B1 -------------------------------------------------------------------------
 
+-- Add a hand to another hand
 (<+) :: Hand -> Hand -> Hand
 (<+) Empty hand = hand
 (<+) (Add card leftHand) rightHand = Add card (leftHand <+ rightHand)
@@ -81,6 +88,7 @@ ranks = [Numeric n | n <- [2..10]] ++ [Jack, Queen, King, Ace]
 suits = [Hearts, Spades, Diamonds, Clubs]
 allCards = [Card rank suit | rank <- ranks, suit <- suits]
 
+-- Return a complete deck of cards
 fullDeck :: Hand
 fullDeck = cardsToHand Empty allCards
   where cardsToHand :: Hand -> [Card] -> Hand
@@ -89,6 +97,7 @@ fullDeck = cardsToHand Empty allCards
 
 -- B3 -------------------------------------------------------------------------
 
+-- Draw the topmost card
 --      Deck    Hand    (Deck, Hand)
 draw :: Hand -> Hand -> (Hand, Hand)
 draw Empty _ = error "draw: The deck is empty"
@@ -96,21 +105,25 @@ draw (Add deckCard deck) hand = (deck, Add deckCard hand)
 
 -- B4 -------------------------------------------------------------------------
 
+
 playBank :: Hand -> Hand
 playBank deck = playBankHelper deck Empty
 
+-- The bank draws cards while it's hand value is less than 16
 playBankHelper :: Hand -> Hand -> Hand
 playBankHelper (Add deckCard deck) hand | value hand < 16 = playBankHelper deck (Add deckCard hand)
                                         | otherwise       = hand
 
 -- B5 -------------------------------------------------------------------------
 
+-- Remove a card of a certain index from a deck
 removeCard :: Hand -> Integer -> (Card, Hand)
 removeCard Empty _                            = error "removeCard error: Empty hand"
 removeCard (Add card deck) 0                  = (card, deck)
 removeCard (Add card deck) index              = (removedCard, (Add card restOfDeck))
   where (removedCard, restOfDeck) = removeCard deck (index - 1)
 
+-- Draw a random card from a deck and add to a new deck
 shuffleDeckAcc :: StdGen -> Hand -> Hand -> Hand
 shuffleDeckAcc _ acc Empty = acc
 shuffleDeckAcc g1 acc deck = shuffleDeckAcc g2 (Add card acc) newDeck
